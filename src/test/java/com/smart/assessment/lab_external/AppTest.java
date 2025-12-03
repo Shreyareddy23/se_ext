1fd440ff8ff7ef1b1f07dbe2462ae4cc19af2231
Shreyareddy23 <shreyareddy076@gmail.com>
2025-12-03 13:26:11 +0530

    hi

---
+++ src/test/java/com/smart/assessment/lab_external/AppTest.java
@@ -0,0 +1,19 @@
+package com.smart.assessment.lab_external;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
